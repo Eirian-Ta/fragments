cf4c26c934acaac75309ba439db24d494a0cf761
Eirian Ta <tta6@myseneca.ca>
2022-06-10 20:32:12 -0400

    configured environment, added fragment model and memory-db

--- src/authorization/index.js
+++ src/authorization/index.js
@@ -1,7 +1,10 @@
 // src/authorization/index.js
 
+if (process.env.NODE_ENV == 'development') {
+  module.exports = require('./basic-auth');
+}
 // Prefer Amazon Cognito
-if (process.env.AWS_COGNITO_POOL_ID && process.env.AWS_COGNITO_CLIENT_ID) {
+else if (process.env.AWS_COGNITO_POOL_ID && process.env.AWS_COGNITO_CLIENT_ID) {
   module.exports = require('./cognito');
 }
 // Also allow for an .htpasswd file to be used, but not in production
